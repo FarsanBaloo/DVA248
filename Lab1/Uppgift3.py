bd08095b07d73058c47c8dce1db2687395a6f3c6
Arosdawn <Freddzork.karlsson@gmail.com>
2023-04-05 16:29:07 +0200

    lab1

--- Lab1/Uppgift3.py
+++ Lab1/Uppgift3.py
@@ -3,13 +3,15 @@ import threading
 
 
 def myMoon(argument, lock):
-    lock.acquire()
-    i = 0
-    while i < 10:
-        print(argument)
-        time.sleep(0.2)
-        i += 1
-    lock.release()
+    while True:
+        lock.acquire()
+        i = 0
+        while i < 10:
+            print(argument)
+            time.sleep(0.2)
+            i += 1
+        lock.release()
+        time.sleep(0.1)
 
 
 def main():
@@ -17,13 +19,15 @@ def main():
     lock = threading.Lock()
     t1 = threading.Thread(target=myMoon, args=(argument, lock), daemon=True)
     t1.start()
-    lock.acquire()
-    i = 0
-    while i < 10:
-        print("Hello World", i)
-        time.sleep(0.5)
-        i += 1
-    lock.release()
+    while True:
+        lock.acquire()
+        i = 0
+        while i < 10:
+            print("Hello World", i)
+            time.sleep(0.5)
+            i += 1
+        lock.release()
+        time.sleep(0.1)
 
 
 if __name__ == '__main__':
